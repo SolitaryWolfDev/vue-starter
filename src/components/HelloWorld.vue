9ca623def453bd094318504cc8c3a0b26fab00fe
Huang Kai <huangkai@192.168.1.12>
2023-01-10 13:18:34 -0600

    boilerplate code for signup, confirm and signin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,92 @@
-<script setup>
+<script>
 import { ref } from 'vue'
+///
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetails} from 'amazon-cognito-identity-js';
 
-defineProps({
-  msg: String,
-})
+const poolData = {
+  UserPoolId: 'us-east-1_LDTH1c0wj',
+  ClientId: '1gtloqe56inp0fkj4mu4p1msq5'
+};
+const userPool = new CognitoUserPool(poolData);
+
+export default {
+  data() {
+    return {
+      username: 'kaihuang',
+      password: 'KaiHuang123!',
+      email: 'sgkaihuang@gmail.com',
+      phoneNumber: '+15555555555',
+      authCode: '574149'
+    }
+  },
+
+  methods: {
+    signUp() {
+      const attributeList = [
+        new CognitoUserAttribute({
+          Name: 'email',
+          Value: this.email
+        }),
+        new CognitoUserAttribute({
+          Name: 'phone_number',
+          Value: this.phoneNumber
+        })
+      ];
+      
+      // username, password are needed, attributes are in a list
+      userPool.signUp(this.username, this.password, attributeList, null, (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      });
+    },
+
+    confirmSignUp() {
+      const cognitoUser = new CognitoUser({
+        Username: this.username,
+        Pool: userPool
+      });
+      
+      cognitoUser.confirmRegistration(this.authCode, true, (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      });
+    },
+
+    signIn() {
+      const authenticationData = {
+        Username: this.username,
+        Password: this.password
+      };
+      const authenticationDetails = new AuthenticationDetails(authenticationData);
+      
+      const cognitoUser = new CognitoUser({
+        Username: this.username,
+        Pool: userPool
+      });
+      
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(result) {
+          console.log(result);
+        },
+        onFailure: function(err) {
+          console.log(err);
+        }
+      });
+    },
+
+    greet() {
+    // `this` inside methods points to the current active instance
+    console.log("hello")
+  }
+  }
+}
+///
 
 const count = ref(0)
 </script>
@@ -18,6 +101,10 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
+
+  <button v-on:click="signUp">Add 2</button>
+  <button v-on:click="confirmSignUp">confirmSignUp</button>
+  <button v-on:click="signIn">SignIn</button>
 
   <p>
     Check out
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export default {
+    region: "us-east-1",
+    IdentityPoolId: "us-east-1_LDTH1c0wj",
+    UserPoolId: "us-east-1_LDTH1c0wj",
+    ClientId: "1gtloqe56inp0fkj4mu4p1msq52"
+  };
